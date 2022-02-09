4e790d5bdae8b52627c29facddb89c44e3f78d17
SB-9 <burgesss@middleton.school.nz>
2022-02-10 09:34:45 +1300

    Update If statement practice.py
    Script to see if you can apply for a learners in NZ

---
+++ If statement practice.py
@@ -0,0 +1,8 @@
+DRIVING_AGE = 16
+age = int(input("what is your age: "))
+if age >= DRIVING_AGE:
+    can_drive = "true"
+else: can_drive = "false"
+if can_drive == "true":
+    print("User can apply for license")
+else: print("user cannot apply for license")
