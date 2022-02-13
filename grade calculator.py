c9db7e8c58830ab7988a1ce758c45783d5c4e7ec
SB-9 <burgesss@middleton.school.nz>
2022-02-14 12:39:09 +1300

    Create grade calculator.py

---
+++ grade calculator.py
@@ -0,0 +1,14 @@
+# grade calculator created by Samuel Burgess on 14'th Febuary 2022
+mark = int(input("Enter mark: "))
+A = 90
+B = 70
+C = 50
+
+if mark >= A:
+    print(f"{mark} is grade A")
+elif mark >= B:
+    print(f"{mark} is grade B")
+elif mark >= C:
+    print(f"{mark} is grade C")
+else:
+    print(f"{mark} is a fail")
