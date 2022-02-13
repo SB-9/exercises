19f8b3dc9643833d2041fe60611812fb49cd3958
SB-9 <burgesss@middleton.school.nz>
2022-02-14 12:12:38 +1300

    Create blood calculator.py

---
+++ blood calculator.py
@@ -0,0 +1,10 @@
+# Blood donor calculator made by Samuel Burgess on 14'th Febuary 2022
+age = float(input("What is your age: "))
+weight = float(input("What is your weight in Kilograms: "))
+
+AGE_LIMIT = 16
+WEIGHT_LIMIT = 50
+if age > AGE_LIMIT and weight < WEIGHT_LIMIT:
+    print("Eligible to donate blood")
+else:
+    print("Not eligible to donate blood")
