1b2da9c094c95067d3f5900d6e4f0492be0c108c
SB-9 <burgesss@middleton.school.nz>
2022-02-14 12:22:57 +1300

    Create odd or even.py
    even or odd checker

---
+++ odd or even.py
@@ -0,0 +1,6 @@
+# odd or even calculator made by Samuel Burgess on 14'th of Febuary 2022
+number = float(input("Please input a number: "))
+if number % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
