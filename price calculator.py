110d31b4eb8f2a2fe065556ca1bc1a4c22a8f6bf
SB-9 <burgesss@middleton.school.nz>
2022-02-14 12:31:22 +1300

    Create price calculator.py

---
+++ price calculator.py
@@ -0,0 +1,12 @@
+# price category checker created by Samuel Burgess on 14'th Febuary 2022
+age = int(input("please input age: "))
+
+TEENAGER_AGE = 13
+ADULT_AGE = 18
+
+if age >= ADULT_AGE:
+    print("Adult price")
+elif age >= TEENAGER_AGE:
+        print("Teenager price")
+else:
+    print("Child price")
