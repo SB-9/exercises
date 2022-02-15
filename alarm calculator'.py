f42cd14b6c4d6800f565c547e1826138d1722d5b
SB-9 <burgesss@middleton.school.nz>
2022-02-15 14:43:17 +1300

    Create alarm calculator'.py

---
+++ alarm calculator'.py
@@ -0,0 +1,8 @@
+# alarm time script created by Samuel Burgess on 15'th feb 2022
+day = int(input("what day is it?\n1-monday... Sunday-7: "))
+if day <= 5:
+    print("alarm rings at 7")
+elif day == 6:
+    print("alarm rings at 8")
+elif day == 7:
+    print("alarm rings at 8")
