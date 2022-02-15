71bac6f5d3f85f89d20dcdb8a536d6b77c073717
SB-9 <burgesss@middleton.school.nz>
2022-02-15 14:36:42 +1300

    Update paracetamol calculator.py

--- paracetamol calculator.py
+++ paracetamol calculator.py
@@ -1,7 +1,7 @@
 # Paracetamol calculator script made by Samuel Burgess on 15'th Feb 2022
 age = int(input("What is your age: "))
-weight = int(input("What is your weight: "))
 if age < 12:
+    weight = int(input("What is your weight: "))
     dose = weight * 10
 else:
     dose = 1000
