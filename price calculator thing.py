0df61ff3f1d7a5157be53e0eb7a86c72dde18480
SB-9 <burgesss@middleton.school.nz>
2022-02-16 09:55:58 +1300

    Update price calculator thing.py

--- price calculator thing.py
+++ price calculator thing.py
@@ -1,10 +1,11 @@
 # price calculator script made by Samuel Burgess on 15'th Feb 2022
 price = float(input("what is the price?: "))
-type = str(input("what item type is it: "))
 
-if price > 10 and type == "food":
+item_type = str(input("what item type is it: "))
+
+if price > 10 and item_type == "food":
     total = (price * 0.6)
-elif price > 10 and type == "eletrical":
+elif price > 10 and item_type == "electrical":
     total = (price * 0.7)
 elif price > 10:
     total = (price * 0.8)
