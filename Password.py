bf135e545c78b4b83bd624dad8c4aee7d9456eb6
SB-9 <burgesss@middleton.school.nz>
2022-02-17 09:08:13 +1300

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,14 +1,22 @@
 # Password script made by Samuel Burgess on 17'th Feb 2022
-
+import sys
 
 password = "frojos"
 access = "false"
+attempts = 0
 
-while access == "false":
-    user_password = input("What is the password: ")
-    if password == user_password:
-        access = "true"
-        print("access granted")
+while access == "false" or attempts != 3:
+    if attempts != 3:
+        user_password = input("What is the password: ")
+        if password == user_password:
+            access = "true"
+            print("access granted")
+            sys. exit()
+        else:
+            attempts += 1
+    else:
+        print("too many attempts")
+        sys. exit()
 
 
 
